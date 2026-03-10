64212b9c67b9fd7e68905e9c7ef121b617142d61
woodori1234 <woodori1234@gmail.com>
2026-03-10 17:30:21 +0900

    chore: bump SW cache version

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Service Worker - Offline Support & Caching
  */
 
-const CACHE_NAME = 'block-puzzle-v3';
+const CACHE_NAME = 'block-puzzle-v4';
 const ASSETS_TO_CACHE = [
     '/',
     'index.html',
